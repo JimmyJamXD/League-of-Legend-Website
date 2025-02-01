f2085d039bb244434c512d0124a07b68a64e6a0f
Jimmy Ngo <70559170+JimmyJamXD@users.noreply.github.com>
2025-01-31 21:15:11 -0800

    Adding JS files

---
+++ main/js/data.js
@@ -0,0 +1,89 @@
+var champData = {
+    "varus" : {
+        "displayName": "Varus",
+        "charaInfo": "The Arrow of Retribution",
+        "related": ["Aatrox", "Kayn"],
+        "image": [
+            {"img":"img/Varus/varus.jpg"},
+            {"img":"img/Varus/varus_artic.jpg"},
+            {"img":"img/Varus/varus_comsic.jpg"},
+            {"img":"img/Varus/varus_dark_star.jpg"},
+            {"img":"img/Varus/varus_project.jpg"}
+
+        ],
+        "Role": [
+            {"heading":"Role: Bot/ADC"},
+            {"desc":"Description: Whether you're playing a Mage or rocking a more traditional Marksman, whoever you're"},
+            {"heading":"Position: Slow Pushing"}
+        ]
+    },
+
+    "zed" : {
+        "displayName": "Zed",
+        "charaInfo": "The Master of Shadows",
+        "related": ["Shen", "Akali", "Kennen"],
+        "image": {"img":"img/zed.jpg"},
+        "Role": [
+            {"heading": "Role: Middle"},
+            {"desc": "To create a Slow Push, you simply need to kill the enemy caster minions when the lane is in a neutral state, or last hit when coming from under your turret. Both generate a minion advantage for you, and through last hitting, you maintain the advantage by relying solely on the minion’s damage to push the wave forward. Naturally, this is a slow process thus the name ‘slow push’. But, generally speaking, you need to have a 2 to 3 minion advantage over your enemy’s wave to generate a slow push if the wave is on your side of the lane. If it’s on the opponent’s, you’ll need a 4 to 5 advantage."}
+        ]
+    },
+
+    "nami" : {
+        "displayName": "Nami",
+        "charaInfo": "The TideCaller",
+        "related": ["Fizz", "Nautilus"],
+        "image": [
+            {"img":"img/Nami/nami.jpg"},
+            {"img":"img/Nami/nami_blossom.jpg"},
+            {"img":"img/Nami/nami_comsic.jpg"},
+            {"img":"img/Nami/nami_koi.jpg"},
+            {"img":"img/Nami/nami_program.jpg"}
+    ],
+        "Role": [
+            {"heading": "Role: Support"},
+            {"desc": "Description: Last, but not least, Supports. Warding is perhaps the biggest thing that every Support player focuses on. But there’s a difference between applying vision and applying effective vision. Many support players will simply run to common ward locations and drop off their totems and sight wards thinking that because they held common angles that they’re doing their jobs. But warding is an adaptive concept, much like fighting, wave manipulation, and gameplanning is for other roles."},
+            {"heading": "Position: Warding/Team Support"},
+            {"desc": "To create a Slow Push, you simply need to kill the enemy caster minions when the lane is in a neutral state, or last hit when coming from under your turret. Both generate a minion advantage for you, and through last hitting, you maintain the advantage by relying solely on the minion’s damage to push the wave forward. Naturally, this is a slow process thus the name ‘slow push’. But, generally speaking, you need to have a 2 to 3 minion advantage over your enemy’s wave to generate a slow push if the wave is on your side of the lane. If it’s on the opponent’s, you’ll need a 4 to 5 advantage."}
+        ]
+    },
+
+    "evelynn" : {
+        "displayName": "Evelynn",
+        "charaInfo": "The Agony's Embrace",
+        "related": ["Vayne", "Tahm Kench", "Ryze"],
+        "image": [
+            {"img":"img/Evelynn/evelynn.jpg"},
+            {"img":"img/Evelynn/evelynn_kda.jpg"},
+            {"img":"img/Evelynn/evelynn_potrait.jpg"},
+            {"img":"img/Evelynn/evelynn_spirit.jpg"},
+            {"img":"img/Evelynn/evelynn_succubus.jpg"}
+        ],
+
+        "Role": [
+            {"heading": "Role: Jungle"},
+            {"desc": "Description: Description: Junglers, the most pressured role in the game, has a lot to be concerned about. Gank pathing, timing, which order they want to clear their camps, how they match against their opposing Jungler and what they can do to win the matchup... All of these things and more go through the minds of a Jungler at the early stages, but overall I believe the core thing that Junglers should focus on is playing towards their game’s win-condition."},
+            {"heading": "Position: Team Layout"},
+            {"desc": "To create a Slow Push, you simply need to kill the enemy caster minions when the lane is in a neutral state, or last hit when coming from under your turret. Both generate a minion advantage for you, and through last hitting, you maintain the advantage by relying solely on the minion’s damage to push the wave forward. Naturally, this is a slow process thus the name ‘slow push’. But, generally speaking, you need to have a 2 to 3 minion advantage over your enemy’s wave to generate a slow push if the wave is on your side of the lane. If it’s on the opponent’s, you’ll need a 4 to 5 advantage."}
+        ]
+    },
+
+    "fiora" : {
+        "displayName": "Fiora",
+        "charaInfo": "The Grand Duelist",
+        "related": ["Jarvan 4", "Garen", "Jax"],
+        "image": [
+            {"img":"img/Fiora/fiora.jpg"},
+            {"img":"img/Fiora/fiora_lunar.jpg"},
+            {"img":"img/Fiora/fiora_pool.jpg"},
+            {"img":"img/Fiora/fiora_project.jpg"},
+            {"img":"img/Fiora/fiora_twin.jpg"}
+        ],
+        "Role": [
+            {"heading": "Role:Top" },
+            {"desc": "Description: Top Lane is a very volatile lane on Summoner’s Rift and has been known for multiple seasons now as the ‘Counterpick Lane’ in Solo Queue. The reason for this is that a lot of Top Lane’s matchups are just flat out decided by who is forced to pick their Champion first. While this doesn’t mean there aren’t ways to win and affect the game through your Champion disadvantage, it does lead to this role being quite snowbally and catering to those that like to outplay their opponents"}
+        ]
+    }
+
+};
+
